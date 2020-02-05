87a889557a67614e353b018ca81cce916dab5011
jiandong.ye <dong100136@outlook.com>
2020-02-05 09:34:28 +0000

    update

--- scripts/CAMELYON16/train_unbanlance.sh
+++ scripts/CAMELYON16/train_unbanlance.sh
@@ -2,20 +2,20 @@
 # python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_10.yaml -d 0
 #
 #----------------------------------------------------------------------------------------
-python3 train.py -c config/unbalance/focal/camelyon16_resnet50_focal_0.yaml 
-python3 train.py -c config/unbalance/focal/camelyon16_resnet50_focal_1.yaml 
-python3 train.py -c config/unbalance/focal/camelyon16_resnet50_focal_2.yaml 
-python3 train.py -c config/unbalance/focal/camelyon16_resnet50_focal_5.yaml 
-
-python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_40.yaml 
-python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_30.yaml 
-python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_20.yaml 
-python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_10.yaml 
-
-python3 train.py -c config/unbalance/weights/camelyon16_resnet50_weights_1_2.yaml 
-python3 train.py -c config/unbalance/weights/camelyon16_resnet50_weights_1_3.yaml 
-python3 train.py -c config/unbalance/weights/camelyon16_resnet50_weights_2_1.yaml 
-python3 train.py -c config/unbalance/weights/camelyon16_resnet50_weights_3_1.yaml 
+# python3 train.py -c config/unbalance/focal/camelyon16_resnet50_focal_0.yaml
+# python3 train.py -c config/unbalance/focal/camelyon16_resnet50_focal_1.yaml
+# python3 train.py -c config/unbalance/focal/camelyon16_resnet50_focal_2.yaml
+# python3 train.py -c config/unbalance/focal/camelyon16_resnet50_focal_5.yaml
+#
+# python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_40.yaml
+# python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_30.yaml
+# python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_20.yaml
+# python3 train.py -c config/unbalance/GHM/camelyon16_resnet50_GHM_10.yaml
+#
+# python3 train.py -c config/unbalance/weights/camelyon16_resnet50_weights_1_2.yaml
+# python3 train.py -c config/unbalance/weights/camelyon16_resnet50_weights_1_3.yaml
+# python3 train.py -c config/unbalance/weights/camelyon16_resnet50_weights_2_1.yaml
+# python3 train.py -c config/unbalance/weights/camelyon16_resnet50_weights_3_1.yaml
 
 #----------------------------------------------------------------------------------------
 python3 train.py -c config/unbalance/focal/camelyon16_densenet161_focal_0.yaml 
