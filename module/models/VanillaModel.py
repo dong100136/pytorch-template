1d15aa94aae5f5441e9d95a43df7b13d8907eb4e
jiandong.ye <dong100136@outlook.com>
2020-02-11 14:55:32 +0800

    add vanilla model

--- module/models/VanillaModel.py
+++ module/models/VanillaModel.py
@@ -3,22 +3,32 @@ import torch.nn.functional as F
 from base import BaseModel
 from ..registry import ARCH
 from module.basic_models.resnet import resnet50
+from module.basic_models.inception import inception_v3
+from module.basic_models.xception import xception
+
 
 @ARCH.register("VanillaModel")
 class VanillaModel(BaseModel):
      def __init__(self, num_classes=10, pretrained=False):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+        self.resnet50 = resnet50(pretrained=pretrained, header=False)
+        self.pooling1 = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.inceptionV3 = inception_v3(pretrained=pretrained, header=False)
+        self.pooling2 = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.xception = xception(pretrained=pretrained,header=False)
+        self.pooling3 = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc1 = nn.Linear(2048, 1024)
+        self.fc2 = nn.Linear(1024, num_classes)
+
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        x1 = self.resnet50(x)
+        x2 = self.inceptionV3(x)
+        x3 = self.xception(x)
+
+        print(x1.shape,x2.shape,x3.shape)
+
+        return x
