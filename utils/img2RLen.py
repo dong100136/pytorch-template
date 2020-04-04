36d8ac31acd2304b254c904c5c23ffb163d3f429
jiandong.ye <dong100136@outlook.com>
2020-04-04 22:44:56 +0800

    recove

--- utils/img2RLen.py
+++ utils/img2RLen.py
@@ -8,13 +8,12 @@ from utils.img_tools import mask2rle
 from tqdm import tqdm
 
 # -------------------------------------------
-img_id_list = list(pd.read_csv("/root/dataset/carvana-image-masking-challenge/test.csv")['images'].values)
+img_id_list = list(pd.read_csv("/root/dataset/carvana-image-masking-challenge/workspace/test.csv")['images'].values)
 img_id_list = [Path(x).name for x in img_id_list]
 base_path = Path("/root/trash/log/prediction/CarNet_v0")
 predict_list = ['%d.npy' % i for i in range(len(img_id_list))]
 # tta = ['mask', 'mask_flip_ud']
-tta = ['mask']
-img_names = [x.stem for x in (base_path / 'mask').glob("*.npy")]
+tta = ['masks']
 output_file = base_path / 'submission.csv'
 threshold = 0.45
 # -------------------------------------------
@@ -30,7 +29,7 @@ def get_img_by_threshold(img, tta=None, threshold=0.5):
     if tta.find('flip_ud') >= 0:
         img = cv2.flip(img, 1)
 
-    mask = np.zeros_like(img, dtype=np.float)
+    mask = np.zeros_like(img, dtype=np.int8)
     mask[img <= threshold] = 0
     mask[img > threshold] = 1
 
@@ -38,12 +37,9 @@ def get_img_by_threshold(img, tta=None, threshold=0.5):
 
 
 for img_name in tqdm(predict_list):
-    if img_name not in img_id_list:
-        continue
-
     masks = []
     for t in tta:
-        img_path = base_path / t / ("%s.npy" % img_name)
+        img_path = base_path / t / img_name
         img = np.load(img_path)
         img = get_img_by_threshold(img, tta=t, threshold=threshold)
         masks.append(img)
@@ -51,10 +47,10 @@ for img_name in tqdm(predict_list):
     predict_mask = np.mean(masks, axis=(0))
 
     rLen = mask2rle(predict_mask)
-    id.append(img_name)
     rle_mask.append(rLen)
     del masks, predict_mask
 
+print(len(img_id_list), ',', len(rle_mask))
 data = pd.DataFrame({
     'img': img_id_list,
     'rle_mask': rle_mask
